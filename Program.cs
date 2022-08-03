ea111bb646b1c1c6fff67c0566421fb2cc26191b
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 22:40:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VowelConsonant
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckVowel.Check();
+        }
+    }
+}
---
+++ Vowel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelConsonant
+{
+    public static class CheckVowel
+    {
+        public static void Check()
+        {
+            Console.WriteLine("Enter any Character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u' ||
+                ch == 'A' || ch == 'E' || ch =='I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is vowel");
+            }
+            else if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+            {
+                Console.WriteLine(ch + " is consonant");
+            }
+        }
+
+    }
+}
